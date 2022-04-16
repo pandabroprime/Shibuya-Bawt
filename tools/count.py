4a02a345259894a1f0ae238bf70a0770c5ea6d4a
pandabroprime <100854877+pandabroprime@users.noreply.github.com>
2022-04-16 16:38:40 -0400

    Add files via upload

--- tools/count.py
+++ tools/count.py
@@ -1,3 +1,4 @@
+#Increases a count by 1.
 def increase(count):
   c = open('data/counts/' + count + '.txt', 'r')
   co = int(c.read())
@@ -7,9 +8,21 @@ def increase(count):
   c.write(str(co))
   c.close()
 
+#Returns a count.
 def get(count):
   c = open('data/counts/' + count + '.txt', 'r')
   co = int(c.read())
   c.close()
   return(co)
- 
+
+#Sets a count to a number specified.
+def set(count, new):
+  c = open('data/counts/' + count + '.txt', 'w')
+  c.write(str(new))
+  c.close()
+
+#Resets a count to 0.
+def reset(count):
+  c = open('data/counts/' + count + '.txt', 'w')
+  c.write(str(0))
+  c.close()
--- tools/list.py
+++ tools/list.py
@@ -1,5 +1,12 @@
+#Turns a .txt file to a list.
 def toList(list):
   f = open('data/lists/' + list + '.txt', 'r')
   l = f.read().split("\n")
   f.close()
   return l
+
+#Adds a new line to a .txt file.
+def add(line, list):
+  f = open('data/lists/' + list + '.txt', 'a')
+  f.write(line + '\n')
+  f.close()
